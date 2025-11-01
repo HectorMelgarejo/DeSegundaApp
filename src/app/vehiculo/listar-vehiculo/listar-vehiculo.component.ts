177ac35943ed87cedf479be336af43601ae2141f
HectorMelgarejo <h.melgarejo@uniandes.edu.com.co>
2025-11-01 09:59:57 -0500

    Agregar conteo por Marca

--- src/app/vehiculo/listar-vehiculo/listar-vehiculo.component.ts
+++ src/app/vehiculo/listar-vehiculo/listar-vehiculo.component.ts
@@ -10,12 +10,22 @@ import { VehiculoService } from '../vehiculo.service';
 export class VehiculoListarComponent implements OnInit {
 
   vehiculos: Vehiculo[] = [];
+  conteoPorMarcaArray: { key: string; value: number }[] = [];
 
   constructor(private vehiculoService: VehiculoService) {}
 
   ngOnInit(): void {
     this.vehiculoService.getVehiculos().subscribe(data => {
       this.vehiculos = data;
+      this.contarPorMarca();
     });
+  }
+
+  contarPorMarca(): void {
+    const conteo: { [marca: string]: number } = {};
+    this.vehiculos.forEach(v => {
+      conteo[v.marca] = (conteo[v.marca] || 0) + 1;
+    });
+    this.conteoPorMarcaArray = Object.entries(conteo).map(([key, value]) => ({ key, value }));
   }
 }
